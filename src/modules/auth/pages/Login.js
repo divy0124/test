cd48fdd5f307e568aadfda3a9346822f8c8ec1c8
Divy <divy@york.ie>
2024-11-14 15:20:52 +0530

    test git patch1

--- src/modules/auth/pages/Login.js
+++ src/modules/auth/pages/Login.js
@@ -1,5 +1,25 @@
-function Login() {
-  return <div>Login</div>;
+import { useState } from 'react';
+
+function Login({ username }) {
+  const [userInput, setUserInput] = useState('');
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Welcome, {username}!</h1>
+      <textarea
+        onChange={handleChange}
+        placeholder="Type something here..."
+        value={userInput}
+      />
+      <p>Output:</p>
+      {/* This is vulnerable to XSS attacks */}
+      <div dangerouslySetInnerHTML={{ __html: userInput }} />
+    </div>
+  );
 }
 
 export default Login;
